be5d7c1755313027587bdc72dc50e82fa2524e53
alves-Moises <moisesingo_ra@hotmail.com>
2021-07-14 23:12:37 -0300

    turtle

--- turtle/main.py
+++ turtle/main.py
@@ -1,8 +1,21 @@
 import turtle
-
+i = 0
 bob = turtle.Turtle()
-bob.fd(100)
-bob.lt(90)
-bob.fd(100)
-bob.rt(120)
-bob.fd(500)
+while i < 10:
+    bob.fd(100)
+    bob.lt(90)
+    #cima
+    bob.fd(100)
+    bob.lt(90)
+    #esquerda
+    bob.fd(100)
+    bob.lt(90)
+    #baixo
+    bob.fd(100)
+    bob.fd(100)
+
+    bob.lt(90)
+    bob.fd(200)
+    bob.lt(90)
+    bob.fd(100)
+    i += 1
